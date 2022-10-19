9897c16bf5a6e5b117e3d047e55aae7b5b4f6bbc
山田怜央 <yamadareo@yamadareiounoMacBook-ea.local>
2022-10-19 17:07:21 +0900

    rspec userモデルスペック追加
    userモデルスペック　パスワード、メールアドレスのテストの追加”

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+  let(:same_email_user) { build(:user, email: user.email) }
+
+  it 'メールアドレス、パスワードがあれば有効な状態であること' do
+    expect(user).to be_valid
+  end
+
+  describe 'パスワードテスト' do
+    it 'パスワードが無ければ無効な状態であること' do
+      user.password = nil
+      expect(user.valid?).to eq false
+    end
+
+    it 'パスワードが６桁以下だと無効な状態であること' do
+      user.password = '12345'
+      expect(user.valid?).to eq false
+    end
+  end
+
+  describe 'メールアドレステスト' do
+    it 'メールアドレスが無ければ無効な状態であること' do
+      user.email = nil
+      expect(user.valid?).to eq false
+    end
+
+    it 'メールアドレスが重複する場合無効な状態であること' do
+      user.save
+      expect(same_email_user.valid?).to eq false
+    end
+  end
 end
