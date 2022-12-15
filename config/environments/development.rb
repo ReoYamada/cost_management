3ff5f395c5ebd3499bc203ddceee91e77d1db1e2
山田怜央 <yamadareo@yamadareiounoMacBook-ea.local>
2022-12-16 05:35:57 +0900

    deviseメールアドレス有効化
    メールアドレスを設定し新規登録時にメール認証

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,14 @@ Rails.application.configure do
   
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => Rails.application.credentials.gmail[:user_name],
+    :password => Rails.application.credentials.gmail[:password],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_130411) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_153533) do
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.integer "unit_price_per_kilo"
@@ -53,6 +53,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_130411) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
